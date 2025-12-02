2485021dbbfb8f81d55035b6b5e8b8c8924ad521
Hassan baig <105589820+baigcoder@users.noreply.github.com>
2025-12-02 15:14:57 +0500

    feat:student and teacher named file added 1

--- intro.tsx
+++ intro.tsx
@@ -1,6 +1,1 @@
-const user_name:string="hassan baig"
-console.log("hassan baig")
-console.log("hy");
-console.log("hy good");
-console.log(user_name)
-// ok done 
+let user_name:string="hassan baig"
